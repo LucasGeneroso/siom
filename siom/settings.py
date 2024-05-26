81156b0330d1e55e9ef82f690873420fab069cbf
Lucas Generoso <lucasgeneroso189@gmail.com>
2024-05-26 20:47:20 -0300

    fix: Models typo (#2)
    * fix: Models typo
    
    * fix: Add missing package in heroku requirements
    
    * chore: Add heroku host to allowed hosts
    
    * chore: Add missing images package
    
    * chore: Minor tweaks
    
    * fix: Static files
    
    * chore: Adjusting static files
    
    * fix: Adjusting location
    
    * chore: Update database
    
    * chore: Remove test branch

--- siom/settings.py
+++ siom/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '0.0.0.0',
     '127.0.0.1',
+    'siom-0287fdd21de0.herokuapp.com'
 ]
 
 
@@ -171,13 +172,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'code/staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = 'code/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
