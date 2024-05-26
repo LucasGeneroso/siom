93206f4d6bfeb8f8c580d533430fd8f8aa07db51
LucasGeneroso <lucasgeneroso189@gmail.com>
2024-05-26 13:29:12 -0300

    chore: Adjusting static files

--- siom/settings.py
+++ siom/settings.py
@@ -174,7 +174,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'code/staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = 'code/static'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'code/static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
